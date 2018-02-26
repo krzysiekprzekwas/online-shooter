4e03adfcfd1a45d895d70265daf7cfa53edf20ec
tomejzen <tomejzen@wp.pl>
2018-02-26 11:04:59 +0100

    added sphere

---
+++ GameServer/MapObjects/MapSphere.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer.MapObjects
+{
+    class MapSphere : MapObject
+    {
+        public MapSphere(double x, double y, double z, double dx, double dy, double dz)
+            : base(x, y, z)
+        {
+
+            DiameterX = dx;
+            DiameterY = dy;
+            DiameterZ = dz;
+        }
+
+        public MapSphere(double x, double y, double z, double diameter)
+            : base(x, y, z)
+        {
+            DiameterX = diameter;
+            DiameterY = diameter;
+            DiameterZ = diameter;
+        }
+
+
+        public double DiameterX { get; set; }
+        public double DiameterY { get; set; }
+        public double DiameterZ { get; set; }
+    }
+}
