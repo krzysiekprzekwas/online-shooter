4400a6f59093a662c55e36dc4e4967584dacd98c
tomejzen <tomejzen@wp.pl>
2018-04-03 13:17:58 +0200

    Created MapQuad

--- GameServer/Models/Ray.cs
+++ GameServer/Models/Ray.cs
@@ -8,7 +8,24 @@ namespace GameServer.Models
     public class Ray
     {
         public Vector3 Origin { get; set; }
-        public Vector3 Direction { get; set; }
+
+        private Vector3 _direction;
+        // Is a vector of length 1
+        public Vector3 Direction
+        {
+
+            get
+            {
+                return _direction;
+            }
+            set
+            {
+                if (value.LengthSquared() != 1)
+                    value = Vector3.Normalize(value);
+
+                _direction = value;
+            }
+        }
 
         public Ray(Vector3 o, Vector3 d)
         {
--- GameServer/Physics/Intersection.cs
+++ GameServer/Physics/Intersection.cs
@@ -79,5 +79,79 @@ namespace GameServer.Physics
                 (position.Y >= c.Position.Y - hh && position.Y <= c.Position.Y + hh) &&
                 (position.Z >= c.Position.Z - hz && position.Z <= c.Position.Z + hz));
         }
+
+        //public static bool CheckIntersection(MapBox box, Ray ray)
+        //{
+
+        //    // 1.
+        //    Vector3 dS21 = S2.sub(S1);
+        //    Vector3 dS31 = S3.sub(S1);
+        //    Vector3 n = dS21.cross(dS31);
+
+        //    // 2.
+        //    Vector3 dR = R1.sub(R2);
+
+        //    float ndotdR = n.dot(dR);
+
+        //    if (Math.abs(ndotdR) < 1e-6f)
+        //    { // Choose your tolerance
+        //        return false;
+        //    }
+
+        //    float t = -n.dot(R1.sub(S1)) / ndotdR;
+        //    Vector3 M = R1.add(dR.scale(t));
+
+        //    // 3.
+        //    Vector3 dMS1 = M.sub(S1);
+        //    float u = dMS1.dot(dS21);
+        //    float v = dMS1.dot(dS31);
+
+        //    // 4.
+        //    return (u >= 0.0f && u <= dS21.dot(dS21)
+        //         && v >= 0.0f && v <= dS31.dot(dS31));
+        //}
+
+        //public class Test
+        //{
+        //    static class Vector3
+        //    {
+        //        public float x, y, z;
+
+        //        public Vector3(float x, float y, float z)
+        //        {
+        //            this.x = x;
+        //            this.y = y;
+        //            this.z = z;
+        //        }
+
+        //        public Vector3 add(Vector3 other)
+        //        {
+        //            return new Vector3(x + other.x, y + other.y, z + other.z);
+        //        }
+
+        //        public Vector3 sub(Vector3 other)
+        //        {
+        //            return new Vector3(x - other.x, y - other.y, z - other.z);
+        //        }
+
+        //        public Vector3 scale(float f)
+        //        {
+        //            return new Vector3(x * f, y * f, z * f);
+        //        }
+
+        //        public Vector3 cross(Vector3 other)
+        //        {
+        //            return new Vector3(y * other.z - z * other.y,
+        //                               z - other.x - x * other.z,
+        //                               x - other.y - y * other.x);
+        //        }
+
+        //        public float dot(Vector3 other)
+        //        {
+        //            return x * other.x + y * other.y + z * other.z;
+        //        }
+        //    }
+            
+        //}
     }
 }
