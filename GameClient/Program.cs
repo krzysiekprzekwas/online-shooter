8c6bb2eb0ade2e9b6bec7cf248d5ba145e78da0d
Krynio <przekwaskrzysiek@gmail.com>
2018-04-22 15:17:56 +0200

    Correct port conflict

--- GameClient/Program.cs
+++ GameClient/Program.cs
@@ -19,7 +19,7 @@ namespace GameClient
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:80", "http://0.0.0.0:80")
+                .UseUrls("http://*:80")
                 .UseStartup<Startup>()
                 .Build();
     }
