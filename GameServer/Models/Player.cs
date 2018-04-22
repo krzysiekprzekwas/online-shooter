2e87178cfccab79111bf40d4c7c9b92c0d168dc3
Krynio <przekwaskrzysiek@gmail.com>
2018-04-22 14:37:05 +0200

    Merge repair

--- GameServer/Models/Player.cs
+++ GameServer/Models/Player.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using GameServer.MapObjects;
+using GameServer.States;
+using Newtonsoft.Json;
 
 namespace GameServer.Models
 {
