c86a812e345c047a3deafce5b9e9497f05146e88
Krynio <przekwaskrzysiek@gmail.com>
2018-04-26 21:09:09 +0200

    Added Nlog configuration

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -7,20 +7,48 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace GameServer
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseUrls("http://*:1000", "http://0.0.0.0:5000")
-                .UseKestrel()
-                .UseStartup<Startup>()
-                .Build();
 
-            host.Run();
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+
+                var host = new WebHostBuilder()
+                    .UseUrls("http://*:1000", "http://0.0.0.0:5000")
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    })
+                    .UseNLog()  // NLog: setup NLog for Dependency injection
+                    .Build();
+
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+
+            
         }
     }
 }
