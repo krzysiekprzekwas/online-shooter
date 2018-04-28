13fb7c13155ce197474ed906e0f4001e45858682
tomejzen <tomejzen@wp.pl>
2018-04-28 23:01:09 +0200

    Updated config to be able to change settings for unit tests

--- GameServer/Config.cs
+++ GameServer/Config.cs
@@ -8,22 +8,23 @@ namespace GameServer
 {
     public class Config
     {
+        // TODO: change static to const
         [JsonProperty]
-        public const int BUFFER_SIZE = 4 * 1024;
+        public static int BUFFER_SIZE = 4 * 1024;
         [JsonProperty]
-        public const int SERVER_TICK = 64;
+        public static int SERVER_TICK = 64;
         [JsonProperty]
-        public const float PLAYER_SPEED = 50; // PER SECOND
+        public static float PLAYER_SPEED = 50; // PER SECOND
         [JsonProperty]
-        public const float GRAVITY = 10; // PER SECOND
+        public static float GRAVITY = 10; // PER SECOND
         [JsonProperty]
-        public const float JUMP_POWER = 2;
+        public static float JUMP_POWER = 2;
         [JsonProperty]
-        public const float PLAYER_DECCELERATION = 0.2f;
+        public static float PLAYER_DECCELERATION = 0.2f;
         [JsonProperty]
-        public const float PLAYER_SIZE = 32;
+        public static float PLAYER_SIZE = 32;
 
-        public const float INTERSECTION_INTERVAL = 0.01f;
+        public static float INTERSECTION_INTERVAL = 0.01f;
 
         private static Config instance;
         public static Config Instance
