f64e77402ae93470078c3af66e31dda81b35672f
Krynio <przekwaskrzysiek@gmail.com>
2018-10-13 18:49:50 +0200

    Move random name as placeholder

--- GameServer/wwwroot/js/app.js
+++ GameServer/wwwroot/js/app.js
@@ -18,28 +18,28 @@ function setup() {
     
     vex.defaultOptions.className = 'vex-theme-top';
 
-    vex.dialog.prompt({
-        message: 'What is Your name little soldier?',
-        placeholder: 'Player',
-        callback: function (value) {
+    $.get(
+        "https://uinames.com/api/",
+        null,
+        function (data) {
 
-            if (value != false) {
-                // Connection last - we may receive response faster than other class initalization
-                connector.initialize(value);
-            }
-            else {
-                $.get(
-                    "https://uinames.com/api/",
-                    null,
-                    function (data) {
+            vex.dialog.prompt({
+                message: 'What is Your name little soldier?',
+                placeholder: data.name,
+                callback: function (value) {
+
+                    if (value != false) {
+                        // Connection last - we may receive response faster than other class initalization
+                        connector.initialize(value);
+                    }
+                    else {
                         connector.initialize(data.name);
                     }
-                );
-            }
-        }
-    });
+                }
+            });
 
-    
+        }
+    );
 }
 
 function draw() {
