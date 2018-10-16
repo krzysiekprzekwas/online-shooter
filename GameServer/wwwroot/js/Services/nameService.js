d3247bf2edf5336f218862e2bff92ff6685eb2b9
Krynio <przekwaskrzysiek@gmail.com>
2018-10-16 23:10:06 +0200

    Refactor

---
+++ GameServer/wwwroot/js/Services/nameService.js
@@ -0,0 +1,21 @@
+﻿const nameService = {
+
+    getRandomName() {
+
+        var name;
+
+        jQuery.ajax({
+            url: 'https://uinames.com/api/?region=england',
+            async: false,
+            success: function (data) {
+                name = data.name;
+            },
+            error: function () {
+                name = "Player" + Math.floor((Math.random() * 10000) + 1);
+            }
+        });
+
+        return name;
+    },
+
+};
