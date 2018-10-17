9f0e6a01b4894be58407ec640fb78f1fcf314bb7
Krynio <przekwaskrzysiek@gmail.com>
2018-10-17 22:35:07 +0200

    Extract drawing controller

---
+++ GameServer/wwwroot/js/Controllers/DrawingController.js
@@ -0,0 +1,73 @@
+﻿function DrawingController() {
+
+    const that = this;
+
+    that.myPlayer;
+
+    that.SetBackground = function (value) {
+        background(value);
+    };
+
+    that.SetMyPlayer = function (player) {
+        that.myPlayer = player;
+    };
+    
+    that.drawMapObjects = function (mapObjects) {
+
+        noFill();
+        mapObjects.forEach((obj, i) => {
+
+            strokeWeight(2);
+            stroke(243, 156, 18);
+            fill(255, 255, 255);
+
+            texture(texturesService.getTexture(obj.textureId));
+            rect(obj.x - that.myPlayer.x, obj.y - that.myPlayer.y, obj.width, obj.height);
+        });
+    };
+
+    that.drawPlayers = function (players) {
+
+        // Draw enemies
+
+        fill(255, 190, 118);
+
+        stroke(0);
+
+        players.except(function (x) { return that.myPlayer !== null && x.id === that.myPlayer.id; }).forEach((player, i) => {
+            
+            push();
+
+            translate(player.x - that.myPlayer.x, player.y - that.myPlayer.y);
+            rotate(player.angle);
+
+            strokeWeight(0);
+            rect(0, 20, 16, 30);
+
+            strokeWeight(4);
+            ellipse(0, 0, player.radius * 2, player.radius * 2);
+
+            pop();
+        });
+
+        // Draw myPlayer
+
+        fill(255, 190, 118);
+
+        stroke(52, 152, 219);
+
+        push();
+        
+        rotate(that.myPlayer.angle);
+
+        strokeWeight(0);
+        rect(0, 20, 16, 30);
+
+        strokeWeight(4);
+        ellipse(0, 0, that.myPlayer.radius * 2, that.myPlayer.radius * 2);
+
+        pop();
+    };
+};
+
+const drawingController = new DrawingController();
--- GameServer/wwwroot/js/world.js
+++ GameServer/wwwroot/js/world.js
@@ -5,14 +5,10 @@ let world = {
         this.mapObjects = Array();
         this.players = Array();
         this.myPlayer = null;
-
-        this.fpsLabel = document.getElementById("fpsLabel");
-        this.pingLabel = document.getElementById("pingLabel");
+        
         this.playerId = -1;
-        this.ping = 999;
         this.receivedGameStateOnCurrentFrame = false;
         this.lastGamestate = null;
-        this.lastFrameTime = new Date();
         this.images = [];
 
         this.printCoordinates = false;
@@ -71,76 +67,14 @@ let world = {
         push();
 
         // myPlayer should be in the center
-        const center = {
-            x: (this.myPlayer !== null) ? this.myPlayer.x : 0,
-            y: (this.myPlayer !== null) ? this.myPlayer.y : 0
-        };
-
-        if (typeof center.x === "undefined" || typeof center.y === "undefined") {
-            console.error("Unknown center of map (probably player position is undefined)");
-        }
-
+        drawingController.SetMyPlayer(this.myPlayer !== null ? this.myPlayer : { x: 0, y: 0 });
+        
         // Draw map components
-        this.drawMapObjects(center);
-        this.drawPlayers(center);
+        drawingController.drawMapObjects(this.mapObjects);
+        drawingController.drawPlayers(this.players, this.myPlayer);
 
         // Restore original state
         pop();
-    },
-
-    drawMapObjects: function (center) {
-
-        noFill();
-        this.mapObjects.forEach((obj, i) => {
-
-            strokeWeight(2);
-            stroke(243, 156, 18);
-            fill(255, 255, 255);
-
-            texture(texturesService.getTexture(obj.textureId));
-            rect(obj.x - center.x, obj.y - center.y, obj.width, obj.height);
-
-            if (world.printCoordinates) {
-
-                strokeWeight(0);
-                textSize(12);
-                fill(236, 240, 241);
-                text(`(${obj.x}, ${obj.y} [${obj.width}x${obj.height}])`, obj.x - center.x, obj.y - center.y);
-            }
-        });
-    },
-
-    drawPlayers: function (center) {
-        
-        this.players.forEach((player, i) => {
-
-            fill(255, 190, 118);
-            if (this.myPlayer !== null && player.id === this.myPlayer.id)
-                stroke(52, 152, 219);
-            else
-                stroke(0);
-
-            push();
-
-            translate(player.x - center.x, player.y - center.y);
-            rotate(player.angle);
-
-            strokeWeight(0);
-            rect(0, 20, 16, 30);
-
-            strokeWeight(4);
-            ellipse(0, 0, player.radius * 2, player.radius * 2);
-
-            pop();
-
-            if (world.printCoordinates) {
-
-                strokeWeight(0);
-                textSize(12);
-                fill(236, 240, 241);
-                text(`(${Math.round(player.x)}, ${Math.round(player.y)} [${player.radius}])`, player.x - center.x, player.y - center.y);
-            }
-        });
     },
 
     
