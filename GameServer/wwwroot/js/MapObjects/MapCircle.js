b9fbf9b64a4a1fa03c28fda4addbeea876265cf4
tomejzen <tomejzen@wp.pl>
2018-10-18 23:29:39 +0200

    Implemented MapObject classes

---
+++ GameServer/wwwroot/js/MapObjects/MapCircle.js
@@ -0,0 +1,40 @@
+const Vector2 = require('../Physics/Vector2.js');
+
+function MapCircle(x, y, radius) {
+
+    const that = this;
+
+    that._position = new Vector2(x, y);
+    that._radius = radius;
+
+    that.GetX = () => that._position.GetX();
+    that.SetX = (value) => that._position.SetX(value);
+
+    that.GetY = () => that._position.GetY();
+    that.SetY = (value) => that._position.SetY(value);
+
+    that.GetPosition = () => that._position;
+    that.SetPosition = (value) => that._position = value;
+
+    that.GetRadius = () => that._radius;
+    that.SetRadius = (value) => that._radius = value;
+
+    that.RadiusSquared = () => Math.pow(that._radius, 2);
+
+    that.Equals = (otherCircle) =>
+        that.GetX() == otherCircle.GetX() &&
+        that.GetY() == otherCircle.GetY() &&
+        that.GetRadius() == otherCircle.GetRadius();
+
+    that.ToString = () => `Circle<x:${that.GetX()} y:${that.GetY()}, r:${that.GetRadius()}>`;
+}
+
+MapCircle.Clone = function(mapCircle) {
+
+    return new MapCircle(mapCircle.GetX(), mapCircle.GetY(), mapCircle.GetRadius());
+};
+
+// Export module
+if (typeof module !== "undefined" && module.hasOwnProperty("exports")) {
+    module.exports = MapCircle;
+}
---
+++ GameServer/wwwroot/js/MapObjects/MapRect.js
@@ -0,0 +1,43 @@
+const Vector2 = require('../Physics/Vector2.js');
+
+function MapRect(x, y, width, height) {
+
+    const that = this;
+
+    that._position = new Vector2(x, y);
+    that._width = width;
+    that._height = height;
+
+    that.GetX = () => that._position.GetX();
+    that.SetX = (value) => that._position.SetX(value);
+
+    that.GetY = () => that._position.GetY();
+    that.SetY = (value) => that._position.SetY(value);
+
+    that.GetPosition = () => that._position;
+    that.SetPosition = (value) => that._position = value;
+
+    that.GetWidth = () => that._width;
+    that.SetWidth = (value) => that._width = value;
+
+    that.GetHeight = () => that._height;
+    that.SetHeight = (value) => that._height = value;
+
+    that.Equals = (otherRect) =>
+        that.GetX() == otherRect.GetX() &&
+        that.GetY() == otherRect.GetY() &&
+        that.GetWidth() == otherRect.GetWidth() &&
+        that.GetHeight() == otherRect.GetHeight();
+
+    that.ToString = () => `Rect<x:${that.GetX()} y:${that.GetY()}, w:${that.GetWidth()}, h:${that.GetHeight()}>`;
+}
+
+MapRect.Clone = function(mapRect) {
+
+    return new MapRect(mapRect.GetX(), mapRect.GetY(), mapRect.GetWidth(), mapRect.GetHeight());
+};
+
+// Export module
+if (typeof module !== "undefined" && module.hasOwnProperty("exports")) {
+    module.exports = MapRect;
+}
