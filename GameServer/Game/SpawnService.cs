136a264ff6a90a51047f41b38faa08b198236579
Tomasz Rutkowski <tomejzen@wp.pl>
2018-10-18 14:26:46 +0200

    [US014] Created SpawnService (#44)
    * Created SpawnService
    
    * Handling when no unoccupied spawnpoints found, added some spawnpoits for map for test
    
    * Defined spawnpoints for main map

---
+++ GameServer/Game/SpawnService.cs
@@ -0,0 +1,53 @@
+﻿using GameServer.Models;
+using GameServer.States;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameServer.Game
+{
+    public class SpawnService
+    {
+        public static List<SpawnPoint> SpawnPoints;
+        private static readonly Random random = new Random();
+
+        public static void SpawnPlayer(Player player)
+        {
+            var spawnPoint = GetRandomSpawnPoint();
+
+            var position = spawnPoint.Position.Clone() as Vector2;
+            player.Position = position;
+        }
+
+        public static SpawnPoint GetRandomSpawnPoint()
+        {
+            if(SpawnPoints == null || SpawnPoints.Count == 0)
+            {
+                throw new NullReferenceException("No spawnpoints loaded");
+            }
+
+            var spawnPoints = SpawnPoints.Where(s => !IsOccupied(s)).ToList();
+            if (spawnPoints.Count == 0)
+                spawnPoints = SpawnPoints;
+
+            var randomIndex = random.Next(spawnPoints.Count);
+            return spawnPoints[randomIndex];
+        }
+
+        public static bool IsOccupied(SpawnPoint spawnPoint)
+        {
+            var currentPlayers = new Player[GameState.Instance.Players.Count];
+            GameState.Instance.Players.CopyTo(currentPlayers);
+
+            foreach(var player in currentPlayers)
+            {
+                var distance = Vector2.Distance(player.Position, spawnPoint.Position);
+                if (distance < player.Radius * 2)
+                    return true;
+            }
+
+            return false;
+        }
+
+    }
+}
