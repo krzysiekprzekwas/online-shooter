e094187aafb0b9ca637c69b428dee465baffff31
tomejzen <tomejzen@wp.pl>
2018-10-30 21:09:49 +0100

    Create bullet object

---
+++ GameServer/wwwroot/js/Physics/Bullet.js
@@ -0,0 +1,30 @@
+﻿function Bullet(playerId) {
+
+    const that = this;
+
+    that._playerId = playerId;
+    that.GetPlayerId = () => that._playerId;
+
+    that._position = new Vector2();
+    that.GetPosition = () => that._position;
+    that.SetPosition = (value) => that._position = value;
+
+    that.GetX = () => that.GetPosition().GetX();
+    that.GetY = () => that.GetPosition().GetY();
+
+    that.SetX = (value) => that._position.SetX(value);
+    that.SetY = (value) => that._position.SetY(value);
+
+    that._speed = new Vector2();
+    that.GetSpeed = () => that._speed;
+    that.SetSpeed = (value) => that._speed = value;
+
+    that._radius = 6;
+    that.GetRadius = () => that._radius;
+    that.SetRadius = (value) => that._radius = value;
+
+    that._angle = 0;
+    that.GetAngle = () => that._angle;
+    that.SetAngle = (value) => that._angle = value;
+
+}
